1b2b48e2af3c6a53b9a58da411f37a8ea66c5309
pulkit <maheshwariloya@gmail.com>
2025-01-06 16:34:17 +0530

    Changes

--- login/screens/Splash.tsx
+++ login/screens/Splash.tsx
@@ -1,6 +1,5 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React, { useContext, useEffect } from 'react'
-import { useAppContext } from '../../AppContext'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Splash({navigation}) {
