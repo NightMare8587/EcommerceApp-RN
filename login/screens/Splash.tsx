63b0997099409eb93c083ec5f11c550ef75f5fdc
pulkit <maheshwariloya@gmail.com>
2025-01-06 12:56:51 +0530

    Modified styles for splash

--- login/screens/Splash.tsx
+++ login/screens/Splash.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Splash() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Splash</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
